55cee01576c63d39691fb29051afc9f919d719ae
《●SILENT ♡LOVER⁴³²●》 <burhanking632@gmail.com>
2025-05-14 10:19:20 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    CAPTION: process.env.CAPTION || "POWERED BY 𝑨𝑾𝑨𝑵 𝒁𝑨𝑫𝑨👑",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M AWAN-X-MEERU-MD WHATSAPP BOT 😊♻️",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_CALL: process.env.ANTI_BAD || "true",
+    PM_BLOCKER: process.env.PM_BLOCKER || "true",
+    ANTI_FAKE: process.env.ANTI_BAD || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+    AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923275028087",
+    OWNER_NAME: process.env.OWNER_NAME || "𝑨𝑾𝑨𝑵 𝒁𝑨𝑫𝑨👑",
+    READ_CMD: process.env.READ_CMD || "true",
+    ANTI_FAKE: process.env.ANTI_FAKE || "true",
+    BOT_NAME: process.env.BOT_NAME || "➺𝑨𝑾𝑨𝑵 𝒁𝑨𝑫𝑨👑࿐",
+    DEV: process.env.DEV || "923275028087",//replace with your whatsapp number        
+    ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'same' if you want to resend deleted message in same chat 
+    STATUS_REACT: process.env.STATUS_REACT || "true",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү 𝑨𝑾𝑨𝑵-𝑿-𝑴𝑬𝑬𝑹𝑼-𝑴𝑫`",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
